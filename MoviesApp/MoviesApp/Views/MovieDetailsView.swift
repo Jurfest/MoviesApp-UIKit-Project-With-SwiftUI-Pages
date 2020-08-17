d19173c601e13970ce4b9187b677c9044587947b
Jurfest <jurfest.apps@gmail.com>
2020-08-16 21:53:42 -0300

    integrating SwiftUI view in UIKit project

---
+++ MoviesApp/MoviesApp/Views/MovieDetailsView.swift
@@ -0,0 +1,22 @@
+//
+//  MovieDetailsView.swift
+//  MoviesApp
+//
+//  Created by Diego Jurfest Ceccon on 16/08/20.
+//  Copyright © 2020 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieDetailsView: View {
+    var body: some View {
+        Text("Movie Details View")
+    }
+}
+
+struct MovieDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailsView()
+    }
+}
